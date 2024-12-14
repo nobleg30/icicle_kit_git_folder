4de0ffe444b4e1c9da1f58de4762d4af61608434
Noble G <gnoble30@gmail.com>
2024-12-14 09:36:01 +0000

    weights

---
+++ weight.py
@@ -0,0 +1,7 @@
+import random
+
+# Generate a random value between 100 and 500 for simulation
+random_value = random.randint(100, 500)
+
+# Print the value in the specified format
+print(f"Weight of the item = {random_value} grams")
